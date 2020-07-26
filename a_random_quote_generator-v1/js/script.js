a8b4d2722a973dfba0c8af4e6561f50c2c2c6583
Jelena Feliciano <68608110+Jaaylena@users.noreply.github.com>
2020-07-26 19:43:56 -0400

    Update script.js

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,13 +10,18 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+// declared varibles 
 let randomQuote;
-let randomNum;
-const quotes = [
+let randQuoteGen;
+let quoteIndex;
+let message;
+let prntquote;  
+// and array of quotes and their authors 
+let quotes = [
   {Quote: 'You educate a man; you educate a man. You educate a woman; you educate a generation.', 
-    author: 'Brigham Young'},
+    Author: 'Brigham Young'},
   {Quote:'A woman without a man is like a fish without a bicycle.', 
-    author: 'Irena Dunn',
+    Author: 'Irena Dunn',
     Citation: 'January 1975'},
   {Quote: 'I know enough to know that no woman should ever marry a man who hated his mother.',
     Author:'Martha Gellhorn', 
@@ -32,26 +37,25 @@ const quotes = [
  * `getRandomQuote` function
 ***/
 // function that randomly selects quotes 
-function getRandomQuote (Quote, Author) {
-  Quote = Math.ceil(Quote);
-  Author = math.floor(Author);
-  RandomNum = math.random() * quotes.length + 1;
-  RandomQuote = randomNum;
-  return Math.floor(math.random() * (quote - author + 1)) + Quote;
-}
-
-
+function getRandomQuote () {
+  quoteIndex = Math.floor(Math.random() * quotes.length);
+    randomQuote = quotes[quoteIndex];
+    return randomQuote;
   
+}
+randQuoteGen = getRandomQuote(quotes);
 
 /***
  * `printQuote` function
 ***/
-
-
-
+function printQuote () {
+  randQuoteGen = getRandomQuote(quotes);
+  message = "<p class = 'quote'> " + quotes.quote + "</p>" + "<p class='source'>" + quotes.source + "</p>";
+  prntQuote = document.querySelector('quotes').innerHTML = message;
+}
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false); 
